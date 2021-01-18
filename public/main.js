551b47a2e3dd507af49466dd13c093aa617384fb
lbugasu <lbugasu@gmail.com>
2021-01-18 14:18:25 -0500

    Adding images

--- public/main.js
+++ public/main.js
@@ -26,7 +26,14 @@ $(document).ready(function () {
 
             <h3>Contact:</h3>
             <p class="contactPerson">${item["Main Contact person "]}</p>
-            
+            <div class="links">
+              <a href="${item["Facebook"]}" target="_blank">
+              <img class="icon"/ src= "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Facebook_circle_pictogram.svg/1200px-Facebook_circle_pictogram.svg.png">
+              </a>
+              <a href="${item["Website"]}">
+              <img class="icon" src="https://raw.githubusercontent.com/lbugasu/cifcapps/master/src/link.svg"/>
+              </a>
+            </div>
           </div>`;
         })
         .join("");
@@ -38,21 +45,60 @@ $(document).ready(function () {
       /**
        * Add the styles
        */
-      // var style = document.createElement("style");
-      // style.innerHTML = `
-      //     div.clubItem {
-      //     width: 18%;
-      //     display: inline-block;
-      //     vertical-align: top;
-      //   }
-      //   h2.clubName {
-      //     font-family: "Archivo Black", sans-serif;
-      //     font-size: 18pt;
-      //     text-align: center;
-      //   }
+      var style = document.createElement("style");
+      style.innerHTML = `
+         div.clubItem h3 {
+          font-size: 18pt;
+          margin: 0;
+          padding: 0;
+          color: #49326b;
+        }
+        div.clubItem p {
+          margin: 1% 0.5% 1.5% 0.5%;
+        }
 
-      //   `;
-      // document.head.appendChild(style);
+        div.clubItem {
+          width: 22%;
+          display: inline-block;
+          vertical-align: top;
+          border: 0;
+          padding: 0.5%;
+          margin: 0.5%;
+          border-radius: 5%;
+          border: 1px solid #49326b;
+        }
+        h2.clubName {
+          font-family: "Archivo Black", sans-serif;
+          font-size: 24pt;
+          text-align: center;
+          color: #49326b;
+        }
+        p.clubDescription {
+          font-size: 18pt;
+        }
+        p.meetingPlace {
+          font-size: 16pt;
+          padding: 0 5% 0 5%;
+        }
+        p.contactPerson {
+          width: 90%;
+          display: inline-block;
+          padding: 0 5% 0 5%;
+          font-size: 16pt;
+        }
+        img.clubCoverImage {
+          width: 100%;
+        }
+        img.icon {
+          width: 30px;
+        }
+        div.links {
+          text-align: center;
+        }
+
+
+        `;
+      document.head.appendChild(style);
     });
   });
 });
