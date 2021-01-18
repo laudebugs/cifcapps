c0df142fd425ef39c2fa789fcff0ea3b7c259184
lbugasu <lbugasu@gmail.com>
2021-01-18 13:55:50 -0500

    Adding links to website and fb

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 $(document).ready(function () {
   const clubs = fetch(
-    "https://cors-anywhere.herokuapp.com/https://nyucifc.herokuapp.com/clubs",
+    "https://lbugasu-cors-proxy.herokuapp.com/https://nyucifc.herokuapp.com/clubs",
     {
       method: "GET",
       mode: "cors",
@@ -9,35 +9,30 @@ $(document).ready(function () {
       credentials: "same-origin",
     }
   );
-
-  clubs.then((result) =>
+  clubs.then((result) => {
     result.json().then((items) => {
-      // eslint-disable-next-line no-undef
-      let cal = $("#nyucifc-calendar")[0];
       let clubList = items
         .map((item) => {
-          let div = document.createElement("div");
-          div.innerHTML = `<h1>${item["name"]}</h1>`;
+          console.log(item.coverImageLink);
           return `
           <div
             class= "clubItem"
           >
-            <h2 class="clubName">${item["name"]}</h2>
-            <p class="clubDescription">${item["description"]}</p>
+            <h2 class="clubName">${item["Name"]}</h2>
+          <img class="clubCoverImage" src="${item["coverImageLink"]}"/>
+            <p class="clubDescription">${item["Description"]}</p>
             <h3>Meeting Place:</h3>
-            <p class="meetingPlace">${item["meeting place"]}</p>
+            <p class="meetingPlace">${item["Meeting Place"]}</p>
 
             <h3>Contact:</h3>
-            <p class="contactPerson">${item["main contact person "]}</p>
+            <p class="contactPerson">${item["Main Contact person "]}</p>
             
           </div>`;
         })
         .join("");
-      console.log(clubList);
       // The Monthly View
       let clubsDiv = $("#clubs")[0];
       clubsDiv.innerHTML = clubList;
-      console.log(clubsDiv);
       //   cal.innerHTML = events;
 
       /**
@@ -58,6 +53,6 @@ $(document).ready(function () {
 
       //   `;
       // document.head.appendChild(style);
-    })
-  );
+    });
+  });
 });
